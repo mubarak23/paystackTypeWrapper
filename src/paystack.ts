4c8efc468149ccae8ad47c16ec3b10ce6c8b5c82
mubarak23 <mubarakaminu340@gmail.com>
2022-06-24 15:13:14 +0100

    bank lists

--- src/paystack.ts
+++ src/paystack.ts
@@ -1,3 +1,24 @@
 import { IPaystackBank } from "../dto/IPaystackBank"
 import axios, { AxiosResponse } from "axios"
 
+export const getBankLists = async (country: string): Promise<IPaystackBank[]> => {
+    const baseUrl = process.env.PAYSTACK_BASE_URL + `/bank/?country=${country}`
+    const headers = {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        'content-type': 'application/json',
+        'cache-control': 'no-cache'
+    }
+   try {
+       const response: AxiosResponse<any> = await axios.get(baseUrl, {headers})
+       if( !response.data && response.status !== 200 ){
+        throw new Error('An error occurred with our third party. Please try again at a later time.')
+       } 
+     const paystackBanks: IPaystackBank[] = response.data.data 
+     return paystackBanks 
+   } catch (e) {
+    throw new Error('An error occurred with our third party. Please try again at a later time.')
+   } 
+
+} 
+
+
