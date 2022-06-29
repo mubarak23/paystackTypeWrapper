9e414e2f9349708283b7f4387f098b59e187389c
mubarak23 <mubarakaminu340@gmail.com>
2022-06-29 21:53:01 +0100

    paystack resolve account

---
+++ dto/IPaystackResolveAccount.ts
@@ -0,0 +1,3 @@
+export interface IPaystackResolveAccount {
+    account_name: string
+}
--- src/paystack.ts
+++ src/paystack.ts
@@ -1,4 +1,5 @@
 import { IPaystackBank } from "../dto/IPaystackBank"
+import {IPaystackResolveAccount } from "../dto/IPaystackResolveAccount";
 import axios, { AxiosResponse } from "axios"
 
 export const getBankLists = async (country: string): Promise<IPaystackBank[]> => {
@@ -21,4 +22,21 @@ export const getBankLists = async (country: string): Promise<IPaystackBank[]> =>
 
 } 
 
-
+export const accountNmeEnqury = async (bankCode: string, accountNumber: string): Promise<IPaystackResolveAccount> => {
+    const baseURL =  process.env.PAYSTACK_BASE_URL + `/bank/resolve?account_number=${accountNumber}&bank_code=${bankCode}`
+  const headers = {
+    Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+    'content-type': 'application/json',
+    'cache-control': 'no-cache'
+  }
+  try{
+    const response: AxiosResponse<any> = await axios.get(baseURL, { headers })
+    if(!response.data && response.status !== 200){
+        throw new Error('An error occurred with our third party. Please try again at a later time.')
+    }
+    const payStackResolveAccount : IPaystackResolveAccount = response.data.data
+    return payStackResolveAccount
+  }catch(e){
+    throw new Error('An error occurred with our third party. Please try again at a later time.')
+  }
+}
